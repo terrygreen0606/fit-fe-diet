50067b68ac86aa50738b0113d4548a1fccabceda
Kirill Savko <kirillsavko25@gmail.com>
2020-07-30 15:17:52 +0300

    remove second arg in ingredients api

--- src/api/ingredients.ts
+++ src/api/ingredients.ts
@@ -1,9 +1,9 @@
 import axios from 'utils/axios';
 
 export const searchIngredients = (token: string, name: string) => {
-  return axios.get(`/ingredient/search/${name}`, {});
+  return axios.get(`/ingredient/search/${name}`);
 };
 
 export const getIngredient = (token: string, id: string) => {
-  return axios.get(`/ingredient/${id}`, {});
+  return axios.get(`/ingredient/${id}`);
 };
