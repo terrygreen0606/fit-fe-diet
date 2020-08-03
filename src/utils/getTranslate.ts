3078bc05547bf5661de6ce371ee282327b92ff21
imgeorgius129 <imgeorgius@gmail.com>
2020-08-03 10:19:10 +0300

    remove placeholders constants

--- src/utils/getTranslate.ts
+++ src/utils/getTranslate.ts
@@ -1,15 +1,10 @@
-import { translatePlaceholders } from 'constants/translatePlaceholders';
-
 export const getTranslate = (phrases: any, code: string, placeholders?: any) => {
   let phrase = phrases[code] || null;
 
   if (phrase && placeholders) {
     Object.keys(placeholders).forEach(plachlderKey => {
-      const placeholder = translatePlaceholders.find(key => key.replace(/#/g, '') === plachlderKey.toString().toUpperCase());
-
-      if (!placeholder) return;
-
-      phrase = phrase.replace(new RegExp(placeholder, 'gi'), placeholders[plachlderKey]);
+      const placeholder = plachlderKey.toString().toUpperCase();
+      phrase = phrase.replace(new RegExp(`#${placeholder}#`, 'gi'), placeholders[plachlderKey]);
     });
   }
 
