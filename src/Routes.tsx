5941d8b8e9ee2114a72b3ea88976fc2ed2242b43
Kirill Savko <kirillsavko25@gmail.com>
2020-08-04 09:04:22 +0300

    remove MainView page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,7 +7,6 @@ import AuthRoute from './components/common/AuthRoute';
 import BasePage from 'components/hoc/BasePage';
 import Layout from 'components/hoc/Layout';
 import LoginView from './views/LoginView';
-import MainView from './views/MainView';
 import NutritionPlanView from './views/NutritionPlanView';
 import WeightGraphicsPage from './views/WeightGraphicsPage';
 import RecipesView from './views/RecipesView';
@@ -16,76 +15,86 @@ import WaterTrackerView from './views/WaterTrackerView';
 import ChangeMealPlanView from './views/ChangeMealPlanView';
 import NotFound from './views/NotFound';
 
-
-
 const Routes = () => (
-    <Switch>
-      <PrivateRoute
-        path="/"
-        component={(props: any) => (
-          <Layout {...props}><MainView {...props} /></Layout>
-        )}
-        exact
-      />
+  <Switch>
+    <PrivateRoute
+      path='/'
+      component={(props: any) => <Layout {...props}></Layout>}
+      exact
+    />
 
-      <PrivateRoute
-        path="/nutrition/plan"
-        component={(props: any) => (
-          <Layout {...props}><NutritionPlanView {...props} /></Layout>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/nutrition/plan'
+      component={(props: any) => (
+        <Layout {...props}>
+          <NutritionPlanView {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <PrivateRoute
-        path="/nutrition/plan/weights"
-        component={(props: any) => (
-          <Layout {...props}><WeightGraphicsPage {...props} /></Layout>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/nutrition/plan/weights'
+      component={(props: any) => (
+        <Layout {...props}>
+          <WeightGraphicsPage {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <PrivateRoute
-        path="/recipe/create"
-        component={(props: any) => (
-          <Layout {...props}><CreateRecipeView {...props} /></Layout>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/recipe/create'
+      component={(props: any) => (
+        <Layout {...props}>
+          <CreateRecipeView {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <PrivateRoute
-        path="/recipes"
-        component={(props: any) => (
-          <Layout {...props}><RecipesView {...props} /></Layout>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/recipes'
+      component={(props: any) => (
+        <Layout {...props}>
+          <RecipesView {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <PrivateRoute
-        path="/water-tracker"
-        component={(props: any) => (
-          <Layout {...props}><WaterTrackerView {...props} /></Layout>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/water-tracker'
+      component={(props: any) => (
+        <Layout {...props}>
+          <WaterTrackerView {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <PrivateRoute
-        path="/plan/change-meal"
-        component={(props: any) => (
-          <Layout {...props}><ChangeMealPlanView {...props} /></Layout>
-        )}
-        exact
-      />
-      
-      <AuthRoute 
-        path="/login" 
-        component={(props: any) => (
-          <BasePage {...props}><LoginView {...props} /></BasePage>
-        )}
-        exact
-      />
+    <PrivateRoute
+      path='/plan/change-meal'
+      component={(props: any) => (
+        <Layout {...props}>
+          <ChangeMealPlanView {...props} />
+        </Layout>
+      )}
+      exact
+    />
 
-      <Route component={(props: any) => <NotFound {...props} />} />
-    </Switch>
+    <AuthRoute
+      path='/login'
+      component={(props: any) => (
+        <BasePage {...props}>
+          <LoginView {...props} />
+        </BasePage>
+      )}
+      exact
+    />
+
+    <Route component={(props: any) => <NotFound {...props} />} />
+  </Switch>
 );
 
 export default withRouter(Routes);
