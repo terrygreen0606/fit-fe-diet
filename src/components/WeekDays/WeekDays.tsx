bba6e41e6ff3f8224c9f0e670c74721bb6573afe
Kirill Savko <kirillsavko25@gmail.com>
2020-08-17 18:43:09 +0300

    update scrolls

--- src/components/WeekDays/WeekDays.tsx
+++ src/components/WeekDays/WeekDays.tsx
@@ -5,37 +5,33 @@ import WeekDayItem, { ItemProps } from './WeekDayItem';
 import './WeekDays.sass';
 
 type WeekDaysProps = {
-  days: ItemProps[],
-  onChange: (any) => void,
-  curDay: string,
-  type: 'radio' | 'checkbox',
-  name?: string,
+  days: ItemProps[];
+  onChange: (any) => void;
+  curDay: string;
+  type: 'radio' | 'checkbox';
+  name?: string;
 };
 
-const WeekDays = ({
-  days, onChange, curDay, type,
-}: WeekDaysProps) => (
-  <section className='d-flex flex-column align-items-center overflow-x-scroll week-days'>
+const WeekDays = ({ days, onChange, curDay, type }: WeekDaysProps) => (
+  <section className='d-flex flex-column align-items-center overflow-scroll week-days'>
     <div className='week-workout col-auto align-self-baseline'>
-      {
-        days.map((day) => (
-          <label key={`${day.number} ${day.value}`}>
-            <input
-              type={type}
-              value={day.value}
-              onChange={onChange}
-              checked={day.value === curDay}
-              disabled={day.disabled}
-            />
-            <WeekDayItem
-              number={day.number}
-              value={day.value}
-              active={day.value === curDay}
-              disabled={day.disabled}
-            />
-          </label>
-        ))
-      }
+      {days.map((day) => (
+        <label key={`${day.number} ${day.value}`}>
+          <input
+            type={type}
+            value={day.value}
+            onChange={onChange}
+            checked={day.value === curDay}
+            disabled={day.disabled}
+          />
+          <WeekDayItem
+            number={day.number}
+            value={day.value}
+            active={day.value === curDay}
+            disabled={day.disabled}
+          />
+        </label>
+      ))}
     </div>
   </section>
 );
