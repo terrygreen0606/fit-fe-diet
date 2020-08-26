b949371eeb6f1782eb172341b15ba45c0872956b
imgeorgius129 <imgeorgius@gmail.com>
2020-08-26 14:24:12 +0300

    getImagePath util

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -1,2 +1,11 @@
-export const getImagePath = (imagePath: string) => (process.env.NODE_ENV === 'development'
-  ? require(`assets/img/${imagePath}`) : `https://fitstg.s3.eu-central-1.amazonaws.com/${imagePath}`);
+export const getImagePath = (imagePath: string) => {
+  let s3ImgPath = imagePath;
+
+  if (s3ImgPath.split('/').length === 1) {
+    s3ImgPath = `img/${imagePath}`;
+  }
+
+  return process.env.NODE_ENV === 'development'
+    ? require(`assets/img/${imagePath}`) 
+    : `https://fitstg.s3.eu-central-1.amazonaws.com/${s3ImgPath}`;
+};
