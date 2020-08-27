d8070620374daeecb22c7a2a08cb2618a8dde96e
imgeorgius129 <imgeorgius@gmail.com>
2020-08-27 13:43:33 +0300

    remove getSignUpTpl deprecated api method

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,6 @@ export * from './auth/userGoogleSignIn';
 export * from './auth/userFacebookSignIn';
 export * from './auth/userFacebookSignUp';
 export * from './auth/userGoogleSignUp';
-export * from './auth/getSignUpTpl';
 export * from './auth/getSignUpData';
 export * from './loadPhrases';
 export * from './ingredients';
