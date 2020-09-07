788a093a9e036e3e6841ed472ce0fcf0809543ed
imgeorgius129 <imgeorgius@gmail.com>
2020-09-07 14:18:28 +0300

    fetch App settings when App Loads

---
+++ src/store/actions/settings.actions.ts
@@ -0,0 +1,3 @@
+export const SET_APP_SETTING = 'SET_APP_SETTING';
+
+export const setAppSetting = (settings: any) => ({ type: SET_APP_SETTING, settings });
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import authReducer from './authentication.reducer';
 import storageReducer from './storage.reducer';
 import localeReducer from './locale.reducer';
+import settingsReducer from './settings.reducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   storage: storageReducer,
   locale: localeReducer,
+  settings: settingsReducer,
 });
 
 export default rootReducer;
