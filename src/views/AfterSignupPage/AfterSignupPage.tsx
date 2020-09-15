a1819613954bda420e425df41c40e8cb9c14c699
imgeorgius129 <imgeorgius@gmail.com>
2020-09-15 19:59:08 +0300

    after signup tpl

--- src/views/AfterSignupPage/AfterSignupPage.tsx
+++ src/views/AfterSignupPage/AfterSignupPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Components
+import Button from 'components/common/Forms/Button';
+
 import './AfterSignupPage.sass';
 
 const AfterSignupPage = (props: any) => {
@@ -57,10 +60,100 @@ const AfterSignupPage = (props: any) => {
               <h5>Fitlope gives you nutritional habits you need to acheve that body.</h5>
 
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              
+              <div className="after-signup-reviews-slider">
+                <div className="after-signup-reviews-slider__item">
+                  <div className="after-signup-reviews-slider__item--img"></div>
+                  <p className="after-signup-reviews-slider__item--descr">I’m realised with my Fitlope transformation. I lost 30kgin 3 month just by changing my eating habbits.</p>
+                  <h6 className="after-signup-reviews-slider__item--author">Janet K., Fitlope user</h6>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </section>
+
+      <section className="after-signup-faq-sect">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              
+              <div className="after-signup-faq-reviews-slider">
+                <div className="after-signup-faq-reviews-slider__controls">
+                  <span className="after-signup-faq-reviews-slider__controls--nav">prev</span>
+                  <span className="after-signup-faq-reviews-slider__controls--nav">next</span>
+                </div>
+
+                <div className="after-signup-faq-reviews-slider__item">
+                  <div className="after-signup-faq-reviews-slider__item--img"></div>
+                  <p className="after-signup-faq-reviews-slider__item--descr"></p>
+                  <h6 className="after-signup-faq-reviews-slider__item--author"></h6>
+                </div>
+              </div>
+
+            </div>
+            <div className="col-6">
+              
+              <h4>Programs vetted by word’s top nutricionists & medical experts</h4>
+
+              <div className="accordion-container">
+                <div className="accordion-item">
+                  <div className="accordion-item-head">Frequantly asked question 1</div>
+                  <div className="accordion-item-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eum, velit nesciunt assumenda, soluta enim, tempore, quae veniam ipsum et a excepturi laboriosam. Veritatis molestiae tenetur soluta quos ipsum necessitatibus!</div>
+                </div>
+
+                <div className="accordion-item">
+                  <div className="accordion-item-head">Frequantly asked question 1</div>
+                  <div className="accordion-item-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eum, velit nesciunt assumenda, soluta enim, tempore, quae veniam ipsum et a excepturi laboriosam. Veritatis molestiae tenetur soluta quos ipsum necessitatibus!</div>
+                </div>
+
+                <div className="accordion-item">
+                  <div className="accordion-item-head">Frequantly asked question 1</div>
+                  <div className="accordion-item-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eum, velit nesciunt assumenda, soluta enim, tempore, quae veniam ipsum et a excepturi laboriosam. Veritatis molestiae tenetur soluta quos ipsum necessitatibus!</div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="after-signup-expect-sect">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              
+              <h4>Transform your body with little to no work at all</h4>
+
+              <div className="advantages-list">
+                <div className="advantages-list-item">Discover your personal Fitlope nutritional  program</div>
+                <div className="advantages-list-item">Follow your personalprogram and track progress with the mobile app</div>
+                <div className="advantages-list-item">Sustain weight by maintaining your new healthy eating habits</div>
+              </div>
+
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </section>  
+
+      <section className="after-signup-start-today-sect">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              
+              <h4 className="sect-title">Start building your dream body today! </h4>
+
+              <p><b>only 1$ for 14 days!</b></p>
+
+              <Button color="primary">Reveal my plan now!</Button>
+
+            </div>
+          </div>
+        </div>
+      </section>  
     </>
   );
 };
