4013383f4eb541e4e03d9fd3103c3f74ce4da1fc
Kirill Savko <kirillsavko25@gmail.com>
2020-09-24 14:05:33 +0300

    BE integration

--- src/views/Settings/SettingsPaymentHistoryView/SettingsPaymentHistoryView.tsx
+++ src/views/Settings/SettingsPaymentHistoryView/SettingsPaymentHistoryView.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Helmet from 'react-helmet';
+import classnames from 'classnames';
 
 import { routes } from 'constants/routes';
 import { getTranslate } from 'utils';
+import { getPaymentHistory } from 'api';
 
 // Components
 import ProfileLayout from 'components/hoc/ProfileLayout';
 import WithTranslate from 'components/hoc/WithTranslate';
 import Breadcrumb from 'components/Breadcrumb';
+import Spinner from 'components/common/Spinner';
 
 import './SettingsPaymentHistoryView.sass';
 
 const SettingsPaymentHistoryView = (props: any) => {
   const t = (code: string) => getTranslate(props.localePhrases, code);
+
+  const [isSpinnerActive, setIsSpinnerActive] = useState<boolean>(true);
+
+  const [paymentHistory, setPaymentHistory] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    let cleanComponent = false;
+    getPaymentHistory().then((response) => {
+      if (!cleanComponent) setPaymentHistory([...response.data.data]);
+    }).finally(() => {
+      if (!cleanComponent) setIsSpinnerActive(false);
+    });
+    return () => cleanComponent = true;
+  }, []);
 
   return (
     <>
@@ -32,71 +49,71 @@ const SettingsPaymentHistoryView = (props: any) => {
       </div>
       <ProfileLayout>
         <div className='payment-history card-bg'>
-          <h5 className='payment-history__title'>
-            {t('app.title.payment_history')}
-          </h5>
-          <div className='payment-history__table'>
-            <div
-              className='
-              payment-history__table-row
-              payment-history__table-head'
-            >
-              <div className='payment-history__table-number'>
-                {t('payment_history.number')}
-              </div>
-              <div className='payment-history__table-date'>
-                {t('payment_history.date')}
-              </div>
-              <div className='payment-history__table-amount'>
-                {t('payment_history.amount')}
-              </div>
-              <div className='payment-history__table-status'>
-                {t('payment_history.status')}
-              </div>
+          {isSpinnerActive ? (
+            <div className='text-center'>
+              <Spinner
+                size='lg'
+                color='#0FC1A1'
+              />
             </div>
-            <div className='payment-history__table-row'>
-              <div className='payment-history__table-number'>
-                25488374
-              </div>
-              <div className='payment-history__table-date'>
-                15.04.2020
-              </div>
-              <div className='payment-history__table-amount'>
-                25$
-              </div>
-              <div className='payment-history__table-status active'>
-                Active
-              </div>
-            </div>
-            <div className='payment-history__table-row'>
-              <div className='payment-history__table-number'>
-                25488374
-              </div>
-              <div className='payment-history__table-date'>
-                15.04.2020
-              </div>
-              <div className='payment-history__table-amount'>
-                25$
-              </div>
-              <div className='payment-history__table-status canceled'>
-                Canceled
-              </div>
-            </div>
-            <div className='payment-history__table-row ended'>
-              <div className='payment-history__table-number'>
-                25488374
-              </div>
-              <div className='payment-history__table-date'>
-                15.04.2020
-              </div>
-              <div className='payment-history__table-amount'>
-                25$
-              </div>
-              <div className='payment-history__table-status ended'>
-                Ended
-              </div>
-            </div>
-          </div>
+          ) : (
+              <>
+                {paymentHistory.length === 0 ? (
+                  <h5 className='payment-history__title'>
+                    {t('payment_history.empty')}
+                  </h5>
+                ) : (
+                    <>
+                      <h5 className='payment-history__title'>
+                        {t('app.title.payment_history')}
+                      </h5>
+                      <div className='payment-history__table'>
+                        <div
+                          className='
+                        payment-history__table-row
+                        payment-history__table-head'
+                        >
+                          <div className='payment-history__table-number'>
+                            {t('payment_history.number')}
+                          </div>
+                          <div className='payment-history__table-date'>
+                            {t('payment_history.date')}
+                          </div>
+                          <div className='payment-history__table-amount'>
+                            {t('payment_history.amount')}
+                          </div>
+                          <div className='payment-history__table-status'>
+                            {t('payment_history.status')}
+                          </div>
+                        </div>
+                        {paymentHistory.map((item) => (
+                          <div
+                            key={item.number}
+                            className={classnames('payment-history__table-row', {
+                              active: item.status_code === 'order.status.active',
+                              ended: item.status_code === 'order.status.ended',
+                              pending: item.status_code === 'order.status.pending',
+                            })}
+                          >
+                            <div className='payment-history__table-number'>
+                              {item.number}
+                            </div>
+                            <div className='payment-history__table-date'>
+                              15.04.2020
+                            </div>
+                            <div className='payment-history__table-amount'>
+                              {item.amount}
+                            </div>
+                            <div className='payment-history__table-status'>
+                              {t(item.status_code)}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </>
+                  )}
+              </>
+            )}
         </div>
       </ProfileLayout>
     </>
