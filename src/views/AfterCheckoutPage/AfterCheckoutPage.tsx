e39a660fd6740f142dd2760544a8cf1f1d7d0f8c
imgeorgius129 <imgeorgius@gmail.com>
2020-10-07 14:20:46 +0300

    checkout thankyou localize

--- src/views/AfterCheckoutPage/AfterCheckoutPage.tsx
+++ src/views/AfterCheckoutPage/AfterCheckoutPage.tsx
@@ -36,11 +36,16 @@ const AfterCheckoutPage = (props: any) => {
           <div className="row">
             <div className="col-xl-10 offset-xl-1">
                 
-              <h4 className="sect-subtitle">Congratulations!</h4>
-              <p>sulle ja su perele sulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perele  sulle ja su perele sulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perelesulle ja su perele</p>
-              <h4 className="mt-4 mt-sm-5 pt-lg-5">Your trial is valid from today until <span className="text-steel-blue">X</span>, and you will be charged Y for the full plan in 7 days.</h4>
-              <p>Check your email for your login details.</p>
-              <Button className="mt-4 mt-sm-5" size="lg" color="primary" block style={{ maxWidth: '580px' }}>Continue to dashboard</Button>
+              <h4 className="sect-subtitle">{t('checkout.thankyou.header_title')}</h4>
+              <p>{t('checkout.thankyou.header_descr')}</p>
+              <h4 className="mt-4 mt-sm-5 pt-lg-5">
+                {t('checkout.thankyou.trial_info', { COUNT: <span className="text-steel-blue">X</span>, AMOUNT: 'Y' })}
+              </h4>
+              <p>{t('checkout.thankyou.trial_subscr')}</p>
+              
+              <Button className="mt-4 mt-sm-5" size="lg" color="primary" block style={{ maxWidth: '580px' }}>
+                {t('checkout.thankyou.button.dashboard')}
+              </Button>
 
             </div>
           </div>
@@ -64,7 +69,7 @@ const AfterCheckoutPage = (props: any) => {
             </div>
             <div className="col-lg-6 offset-lg-1 after-checkout-app-sect-col-2 text-center">
               
-              <h4 className="mb-5 pb-4">Please click one of the two buttons below to add our mobile app to your home screen</h4>
+              <h4 className="mb-5 pb-4">{t('checkout.thankyou.application_title')}</h4>
 
               <img src={getImagePath('point-arrow-yellow.png')} alt="" className="after-checkout-app-sect-arrow" />
 
@@ -98,7 +103,7 @@ const AfterCheckoutPage = (props: any) => {
             <div className="col-lg-7 order-lg-1 mt-5 mt-lg-0 text-center">
 
               <div className="after-checkout__invite_email__wrap">
-                <h4 className="mb-5 text-steel-blue">Share your membership with your friends</h4>
+                <h4 className="mb-5 text-steel-blue">{t('checkout.thankyou.share_title')}</h4>
                 <InviteEmail className="after-checkout__invite_email" />
               </div>
 
@@ -116,7 +121,9 @@ const AfterCheckoutPage = (props: any) => {
               </div>
 
               <div className="mt-5 pt-md-5">
-                <Button className="after-checkout__dashboard_btn" size="lg" color="primary" block style={{ maxWidth: '700px' }}>Continue to your personal dashboard</Button>
+                <Button className="after-checkout__dashboard_btn" size="lg" color="primary" block style={{ maxWidth: '700px' }}>
+                  {t('checkout.thankyou.button.personal_dashboard')}
+                </Button>
               </div>
 
             </div>
