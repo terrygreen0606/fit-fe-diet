a20aec195e5ad2058414c43072bb4eb72db88a08
Kirill Savko <kirillsavko25@gmail.com>
2020-11-03 13:04:09 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/.ssh/fit-staging.pem ubuntu@ec2-3-94-178-185.compute-1.amazonaws.com 'cd deploy-app && dep deploy --branch=sentry'
+ssh -i ~/.ssh/fit-staging.pem ubuntu@ec2-3-94-178-185.compute-1.amazonaws.com 'cd deploy-app && dep deploy'
