b9448a37bb072c965cb190b8b04cbaf80d9cf872
Kirill Savko <kirillsavko25@gmail.com>
2020-11-04 12:18:00 +0300

    update axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-let baseURL = null;
+let baseURL = '';
 
-if (process.env.NODE_ENV !== 'development') {
-  baseURL = 'https://stgby.fitlope.com/api';
-} else {
+if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:8080/api';
+} else {
+  baseURL = 'https://stgby.fitlope.com/api';
 }
 
 const instance = axios.create({
