9e61481394aa1d47ab5d599ca4886cd2cca2d8f8
Kirill Savko <kirillsavko25@gmail.com>
2020-11-10 10:19:24 +0300

    fix styling issues

--- src/components/hoc/ProfileLayout/ProfileLayout.tsx
+++ src/components/hoc/ProfileLayout/ProfileLayout.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // Components
 import ProfileSidebar from 'components/ProfileSidebar';
+import useWindowSize from 'components/hooks/useWindowSize';
+import useDebounce from 'components/hooks/useDebounce';
 
 import './ProfileLayout.sass';
 
-const ProfileLayout = ({ children }) => {
-  return (
-    <section className="profile-layout">
-      <div className="container">
-        <div className="row">
-          <div className="col-xl-4">
-            
-            <ProfileSidebar />
+type ProfileLayoutProps = {
+  children: any;
+};
 
+const ProfileLayout = ({ children }: ProfileLayoutProps) => {
+  const { width } = useWindowSize();
+  const debouncedWidth = useDebounce(width, 500);
+
+  const layoutWrap = useRef(null);
+
+  useEffect(() => {
+    if (debouncedWidth < 1200 && children) {
+      layoutWrap.current.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+    }
+  }, [debouncedWidth, children]);
+
+  return (
+    <section className='profile-layout'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xl-4'>
+            <ProfileSidebar />
           </div>
-          <div className="col-xl-8 mt-3 mt-xl-0 profile-layout-content-col">
+          <div className='col-xl-8 mt-3 mt-xl-0 profile-layout-content-col' ref={layoutWrap}>
 
             {children}
 
