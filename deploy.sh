3f353e6a788ae0faf0cecfb3ee75f2fb1e145bfe
Kirill Savko <kirillsavko25@gmail.com>
2020-11-16 16:22:13 +0300

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/.ssh/fit-staging.pem ubuntu@ec2-3-94-178-185.compute-1.amazonaws.com 'cd deploy-app && dep deploy'
+ssh -i ~/.ssh/fit-staging-2.pem ubuntu@ec2-3-94-178-185.compute-1.amazonaws.com 'cd deploy-app && dep deploy'
