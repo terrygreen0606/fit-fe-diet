fa337008abafcc14c51c1aa744e36268aab85ebd
Kirill Savko <kirillsavko25@gmail.com>
2020-11-19 12:43:33 +0300

    remove console.log

--- src/views/AfterCheckoutPage/AfterCheckoutPage.tsx
+++ src/views/AfterCheckoutPage/AfterCheckoutPage.tsx
@@ -39,8 +39,6 @@ const AfterCheckoutPage = ({
   useEffect(() => {
     orderAmount = location.orderAmount;
     orderTransactionId = location.orderTransactionId;
-    console.log('orderAmount', orderAmount);
-    console.log('orderTransactionId', orderTransactionId);
     setTimeout(() => {
       window.history.pushState(null, null, '#');
     }, 100);
