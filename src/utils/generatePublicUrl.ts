a9e009950fc8d40d1362aca0fcc2ca1b81f03e91
Kirill Savko <kirillsavko25@gmail.com>
2020-11-27 09:49:54 +0300

    update generatePublicUrl function

--- src/utils/generatePublicUrl.ts
+++ src/utils/generatePublicUrl.ts
@@ -1,1 +1,6 @@
-export const generatePublicUrl = (route: string) => (`https://stgby.fitlope.com/${route}`);
+export const generatePublicUrl = (route: string) => {
+  if (process.env.REACT_APP_ENV === 'production') {
+    return `https://fitlope.com/${route}`;
+  }
+  return `https://stgby.fitlope.com/${route}`;
+};
