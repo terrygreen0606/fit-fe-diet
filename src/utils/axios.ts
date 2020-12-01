0a991897ee14b158427e7640afe98605c2443c2f
Kirill Savko <kirillsavko25@gmail.com>
2020-12-01 13:39:56 +0300

    ip and lang parameters

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
 import { getApiBaseUrl } from 'utils';
+import queryString from 'query-string';
 
 const baseURL = getApiBaseUrl();
 
@@ -18,5 +20,24 @@ if (process.env.REACT_APP_ENV === 'production') {
 }
 
 const instance = axios.create(axiosInstanceConfig);
+
+instance.interceptors.request.use((request) => {
+  const queryParametersObj = queryString.parse(window.location.search);
+
+  request.params = request.params || {};
+
+  if (queryParametersObj.lang && queryParametersObj._by_ip) {
+    request.params['lang'] = queryParametersObj.lang;
+    request.params['_by_ip'] = queryParametersObj._by_ip;
+  } else if (queryParametersObj.lang) {
+    request.params['lang'] = queryParametersObj.lang;
+  } else if (queryParametersObj._by_ip) {
+    request.params['_by_ip'] = queryParametersObj._by_ip;
+  }
+
+  return request;
+}, (error) => {
+  throw error;
+});
 
 export default instance;
