d30eac9b01471c6d3ebb9ce9030d02b162f680cb
imgeorgius129 <imgeorgius@gmail.com>
2020-12-01 13:59:35 +0300

    validator util

--- src/utils/FormValidator.ts
+++ src/utils/FormValidator.ts
@@ -133,7 +133,7 @@ const FormValidator = ({ localePhrases = {} }) => {
             result[m] = value && value.length > 0 ? !validator.isInt(value) : false;
             break;
           case 'regex':
-            result[m] = value && value.length > 0 ? !(new RegExp(param).test(value.replaceAll('_', ''))) : false;
+            result[m] = value && value.length > 0 ? !(new RegExp(param).test(value?.replaceAll('_', '') || '')) : false;
             break;
           case 'alphanum':
             result[m] = value && value.length > 0 ? !validator.isAlphanumeric(value) : false;
